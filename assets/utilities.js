646cbd102f7977f81791a036a313199ed55dabc9
hieu7654321 <minhhieu2952004@gmail.com>
2026-05-15 14:16:51 +0700

    LivanHome-Header: Update Schema Settings and Scss

--- assets/utilities.js
+++ assets/utilities.js
@@ -708,7 +708,7 @@ export function setHeaderMenuStyle() {
     window.requestAnimationFrame(() => {
       const overflowList = headerComponent?.querySelector('overflow-list');
       const hasReachedMinimum = overflowList && overflowList.hasAttribute('minimum-reached');
-      headerComponent.dataset.menuStyle = isTouchDevice() || hasReachedMinimum ? 'drawer' : 'menu';
+      headerComponent.dataset.menuStyle = hasReachedMinimum ? 'drawer' : 'menu';
     });
   }
 }
